c4103aeb423129f7367c4f0aab4b2bedb4c0d739
MakeYourSchool <paul@creators-collective.com>
2020-03-21 01:20:29 +0100

    Create Lautstaerkesensor.ino

---
+++ examples/Lautstaerkesensor/Lautstaerkesensor.ino
@@ -0,0 +1,15 @@
+//This example code was originally published on the official Seeed Studio Wiki Website: http://wiki.seeedstudio.com/Grove-Loudness_Sensor/
+
+int loudness;
+
+void setup()
+{
+    Serial.begin(9600);
+}
+
+void loop()
+{
+    loudness = analogRead(0);
+    Serial.println(loudness);
+    delay(200);
+}
